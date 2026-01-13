9cd79c94f1d4c5ea2c9b46dcafed5da8d8f78f7e
codingirl99 <105598595+codingirl99@users.noreply.github.com>
2026-01-12 22:56:45 -0600

    Update analysis.py

---
+++ notebooks/analysis.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#load the dataset
+df = pd.read_csv("detailed_ev_charging_stations.csv")
+
+#find the shape and data types
+print(df.shape)
+print(df.info())
+
+#viewing the first 5 rows
+print(df.head())
+
+# Summary statistics
+print(df.describe())
+
+# Check missing values
+print(df.isna().sum())
+
+# Pie Plot by Charger Type
+labels = ['AC Level 1', 'AC Level 2', 'DC Fast Charger']
+sizes = pd.Series(labels).value_counts()
+fig, ax = plt.subplots()
+ax.pie(sizes, labels = labels, autopct = '%1.1f%%')
+plt.show()
